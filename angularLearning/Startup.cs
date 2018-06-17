e336172e9dced40fac6dac8470d6283e182ace07
Burhan <burhan.hasan.dev@gmail.com>
2018-06-17 18:39:51 +0400

    base files created

--- angularLearning/Startup.cs
+++ angularLearning/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace angularLearning
 {
@@ -37,7 +39,19 @@ namespace angularLearning
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            // подключаем файлы по умолчанию
+            app.UseDefaultFiles();
+            // подключаем статические файлы
             app.UseStaticFiles();
+            // добавляем поддержку каталога node_modules
+            app.UseFileServer(new FileServerOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "node_modules")
+                ),
+                RequestPath = "/node_modules",
+                EnableDirectoryBrowsing = false
+            });
 
             app.UseMvc(routes =>
             {
