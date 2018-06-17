6cfbd50b48bb0939aab55c80397c41787ba2a775
Burhan <burhan.hasan.dev@gmail.com>
2018-06-17 18:27:17 +0400

    recreating project

--- angularLearning/Program.cs
+++ angularLearning/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace angular_js
+namespace angularLearning
 {
     public class Program
     {
