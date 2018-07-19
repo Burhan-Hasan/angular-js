48a8e00a4fc248306594127caefffb499e9b72b9
Burhan <burhan.hasan.dev@gmail.com>
2018-07-19 17:32:22 +0400

    Custom Validator

--- angularLearning/wwwroot/app/components/mdf/mdf.component.ts
+++ angularLearning/wwwroot/app/components/mdf/mdf.component.ts
@@ -1,25 +1,25 @@
 ﻿import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { rangeValidator } from '../../custom_validators/range.validator';
+
 
 @Component({
     moduleId: module.id,
-    selector:'mdf',
-    templateUrl:'mdf.component.html'
+    selector: 'mdf',
+    templateUrl: 'mdf.component.html'
 })
-export class MdfComponent
-{
+export class MdfComponent {
     registrationForm: FormGroup;
 
     ngOnInit() {
         this.registrationForm = new FormGroup({
             username: new FormControl("max", [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
-            age: new FormControl("", [Validators.required, Validators.min(10), Validators.max(100)]),
+            age: new FormControl("", [Validators.required, rangeValidator(10, 100)]),
             password: new FormControl("", [Validators.required])
         });
     }
 
-    onSubmit(registrationForm: any)
-    {
+    onSubmit(registrationForm: any) {
         console.log(registrationForm);
         console.log(registrationForm.valid);
         console.log(registrationForm.value);
---
+++ angularLearning/wwwroot/app/custom_validators/range.validator.ts
@@ -0,0 +1,12 @@
+﻿import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function rangeValidator(min: number, max: number): ValidatorFn
+{
+    return function (control: AbstractControl): { [key: string]: any } {
+        let value = +control.value;
+        if (isNaN(value) || (value < min || value > max))
+            return { 'rangevalidator': { value } };
+        else
+            return null;
+    }
+}
